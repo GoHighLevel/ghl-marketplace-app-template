26746f7a2577b86e2718308781a499ccfd0e6ce5
abhinavgohighlvel <abhinav.paliwal@gohighlevel.com>
2023-10-08 22:37:22 +0530

    Setup basic app template for GHL app

---
+++ src/index.ts
@@ -0,0 +1,108 @@
+/* The code you provided is a TypeScript code that sets up an Express server and defines several routes
+for handling HTTP requests. */
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+import { GHL } from "./ghl";
+
+const path = __dirname + "/ui/dist/";
+
+dotenv.config();
+const app: Express = express();
+
+/* The code `app.use(express.static(path));` is setting up a middleware in the Express server. The
+`express.static` middleware is used to serve static files such as HTML, CSS, JavaScript, and images. */
+app.use(express.static(path));
+
+/* The line `const ghl = new GHL();` is creating a new instance of the `GHL` class. It is assigning
+this instance to the variable `ghl`. This allows you to use the methods and properties defined in
+the `GHL` class to interact with the GoHighLevel API. */
+const ghl = new GHL();
+
+const port = process.env.PORT;
+
+/* The code `app.get("/authorize-handler", async (req: Request, res: Response) => { ... })` sets up an example how you can authorization requests */
+app.get("/authorize-handler", async (req: Request, res: Response) => {
+  const { code } = req.query;
+  await ghl.authorizationHandler(code as string);
+  res.redirect("https://app.gohighlevel.com/");
+});
+
+/* The code `app.get("/example-api-call", async (req: Request, res: Response) => { ... })` shows you how you can use ghl object to make get requests
+ ghl object in abstract would handle all of the authorization part over here. */
+app.get("/example-api-call", async (req: Request, res: Response) => {
+  if (ghl.checkInstallationExists(req.query.companyId as string)) {
+    try {
+      const request = await ghl
+        .requests(req.query.companyId as string)
+        .get(`/users/search?companyId=${req.query.companyId}`, {
+          headers: {
+            Version: "2021-07-28",
+          },
+        });
+      return res.send(request.data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  return res.send("Installation for this company does not exists");
+});
+
+/* The code `app.get("/example-api-call-location", async (req: Request, res: Response) => { ... })` shows you how you can use ghl object to make get requests
+ ghl object in abstract would handle all of the authorization part over here. */
+app.get("/example-api-call-location", async (req: Request, res: Response) => {
+  /* The line `if(ghl.checkInstallationExists(req.params.locationId)){` is checking if an
+    installation already exists for a specific location. It calls the `checkInstallationExists`
+    method of the `GHL` class and passes the `locationId` as a parameter. This method checks if
+    there is an existing installation for the provided locationId and returns a boolean value
+    indicating whether the installation exists or not. */
+  if (ghl.checkInstallationExists(req.params.locationId)) {
+    try {
+      const request = await ghl
+        .requests(req.query.locationId as string)
+        .get(`/contacts/?locationId=${req.query.locationId}`, {
+          headers: {
+            Version: "2021-07-28",
+          },
+        });
+      return res.send(request.data);
+    } catch (error) {
+      console.log(error);
+    }
+  } else {
+    /* The line `await ghl.getLocationTokenFromCompanyToken(req.query.companyId as string,
+        req.query.locationId as string)` is calling the `getLocationTokenFromCompanyToken` method of the
+        `GHL` class. This method is used to retrieve the location token for a specific location within a
+        company. */
+    await ghl.getLocationTokenFromCompanyToken(
+      req.query.companyId as string,
+      req.query.locationId as string
+    );
+    try {
+      const request = await ghl
+        .requests(req.query.locationId as string)
+        .get(`/contacts/?locationId=${req.query.locationId}`, {
+          headers: {
+            Version: "2021-07-28",
+          },
+        });
+      return res.send(request.data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+});
+
+/* The code `app.get("/", function (req, res) {
+  res.sendFile(path + "index.html");
+});` sets up a route for the root URL ("/") of the server.  This is
+ used to serve the main HTML file of a web application. */
+app.get("/", function (req, res) {
+  res.sendFile(path + "index.html");
+});
+
+/* The code `app.listen(port, () => {
+  console.log(`GHL app listening on port `);
+});` is starting the Express server and making it listen on the specified port. */
+app.listen(port, () => {
+  console.log(`GHL app listening on port ${port}`);
+});
